b5a4926e889941ccb5ac01626a7576857de3d367
yonisGit <yoniel@post.bgu.ac.il>
2019-03-29 15:58:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import requests as req
+from colors import red, green
+
 
 #ip = 'http://132.73.201.223'
 def main():
@@ -26,7 +28,7 @@ def main():
     return
 
 def test1(driver):
-    driver.get("http://132.73.201.223:12345/pick_aq/")
+    driver.get("http://10.0.0.3:12345/pick_aq/")
     driver.find_element_by_id('finish_pick_aq').click()
     if(driver.current_url=='http://132.73.201.223:12345/attractions/'):
         print('test passed!')
@@ -36,7 +38,7 @@ def test1(driver):
     return
 
 def test2(driver):
-    driver.get('http://132.73.201.223:12345/attractions/')
+    driver.get('http://10.0.0.3:12345/attractions/')
     driver.execute_script("shitToDeleteFast()")
     print(driver.current_url)
 
@@ -44,7 +46,7 @@ def test2(driver):
 
 
 def test3(driver):
-    driver.get('http://132.73.201.223:12345/attractions/')
+    driver.get('http://10.0.0.3:12345/attractions/')
     res = driver.execute_script("let x= funcThatReturnsOne();"
                                 "return x;")
     print(res)
@@ -52,9 +54,10 @@ def test3(driver):
 
 
 def test_req_one_one(driver):
-    driver.get("http://132.73.201.223:12345/attractions/")
+    print("Test: Add Attraction test.")
+    driver.get("http://10.0.0.3:12345/attractions/")
     first_len = driver.execute_script("return attr_arr_for_test2.length;")
-    print(first_len)
+    # print(first_len)
     driver.find_element_by_id('add_manually_menu').click()
     driver.find_element_by_id('manual_lat').send_keys('31.2625444444')
     driver.find_element_by_id('manual_lng').send_keys('34.8019111199')
@@ -63,25 +66,29 @@ def test_req_one_one(driver):
     driver.find_element_by_id('desc').send_keys('test attraction')
     driver.find_element_by_id('submit_btn_add_attr').click()
     driver.find_element_by_id('skip_game_btn').click()
+    # driver.find_element_by_id('ques').send_keys('?')
+    # driver.find_element_by_id('ans1').send_keys('a')
+    # driver.find_element_by_id('ans2').send_keys('b')
+    # driver.find_element_by_id('ans3').send_keys('c')
+    # driver.find_element_by_id('ans4').send_keys('d')
+    # driver.find_element_by_id('correctAns').send_keys('0')
+    # print("1: "+driver.current_url)
     driver.find_element_by_id('finish_add_aq').click()
+    # print("2: " + driver.current_url)
     driver.find_element_by_id('finish_add_hint').click()
-
-    if driver.current_url == 'http://132.73.201.223:12345/attractions/':
-        print("we are on the right page!")
+    # print("3: " + driver.current_url)
+    #
+    # if driver.current_url == 'http://10.0.0.3:12345/attractions/':
+    #     print("we are on the right page!")
 
     second_len = driver.execute_script("getRequestAttractions(funcForTest);"
-                                      "return attr_arr_for_test.length;")
-    print(second_len)
+                                      "return attr_arr_for_test2.length;")
+    # print(second_len)
 
     if second_len == first_len+1:
-        print("test passed!!!")
+        print(green('--- test passed!!! ---'))
     else:
-        print("test failed ! :( ")
-
-
-
-
-
+        print(red('--- test failed!!! ---'))
 
 
     return
